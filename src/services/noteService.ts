59339d9322977efe3f9229293fa48e2d3e240340
Oleg Shapoval <olegkazhan@gmail.com>
2025-08-21 14:43:40 +0300

    init

---
+++ src/services/noteService.ts
@@ -0,0 +1,12 @@
+// import * as axios from "axios";
+
+// interface NoteServiceProps {
+//   fetchNotes: () => void;
+// }
+//
+// export default function NoteService() {
+//   async function fetchNotes(): void {
+//     const res = await axios.get<Note[]>();
+//   }
+//   return;
+// }
